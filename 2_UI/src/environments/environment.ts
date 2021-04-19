fcf2b4cd0f096cc8ac9485a33b15114428c0b76f
medoesdev <46957771+medoesdev@users.noreply.github.com>
2021-04-18 22:49:28 -0700

    Update environment.ts
    Updated the ticketAPI

--- 2_UI/src/environments/environment.ts
+++ 2_UI/src/environments/environment.ts
@@ -8,15 +8,16 @@ export const environment = {
 
   // TODO: This id can be retrieved in output section of the cognito ui
   // cloud formation stack.
-  cognitoIdentityPoolId: 'eu-west-1:2ecf0f8a-932b-47c6-91cf-2003c9f45f8e',
+  cognitoIdentityPoolId: 'eu-west-1:c5bd44b2-6168-4bc7-99da-6311302db79d',
 
   // TODO: Facebook app id can be retrieved from the application in your
   // facebook developer account.
-  facebookAppId: '2565202357115339',
+  facebookAppId: '312947186914224',
 
   // TODO: The API URL is available in the API Gateway console under Stage
   // NOTE: don't forget trailing "/"  For example:
   // https://api.example.com/prod/
-  ticketAPI: 'https://dsyy28yf9e.execute-api.eu-west-1.amazonaws.com/prod/'
+  ticketAPI: 'https://api.gopay.click/'
 
 };
+
